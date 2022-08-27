b56fed260a01bd50ff3e8e9659c2c32d2aed7aed
Mahyarazad <mhyrinc@gmail.com>
2022-08-27 16:04:19 +0430

    SweetAlert 2 Added & UI Update

--- RecipeApp/wwwroot/blazor-modal.js
+++ RecipeApp/wwwroot/blazor-modal.js
@@ -30,4 +30,17 @@ function AutoHeight() {
     });
 }
 
+function DeleteTagPrompt() {
+    return Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        return result.value;
+    });
+}
 
